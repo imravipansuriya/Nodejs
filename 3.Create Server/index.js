ec51a7c2b8bd567d2510fea04443e24638c8cc76
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-06-11 11:22:58 +0530

    node js

--- 3.Create Server/index.js
+++ 3.Create Server/index.js
@@ -1,3 +1,16 @@
 const http = require('http')
 
-http.createServer().listen(4500)
+
+const dataControle = (req, res) =>{
+    res.write("Hrllo this is ravi pansuriya");
+    res.end()
+}
+
+http.createServer(dataControle).listen(4500)
+
+
+
+// http.createServer( (req, res) =>{
+//     res.write("Hrllo this is anil sidhu");
+//     res.end()
+// }).listen(4500)
